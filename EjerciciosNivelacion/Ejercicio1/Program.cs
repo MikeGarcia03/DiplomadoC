cd4b31390b12cd0b445b0834ae442b1515516b5a
MikeGarcia03 <miguelangelgarciadesena@gmail.com>
2019-10-08 22:00:45 -0400

    Comentarios agregados al programa del proyecto Ejercicio1, creacion del proyecto Ejercicio2

--- EjerciciosNivelacion/Ejercicio1/Program.cs
+++ EjerciciosNivelacion/Ejercicio1/Program.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Programa que muestra los datos personales
 namespace Ejercicio1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Estableciendo los colores de las letras y el fondo de la consola
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Red;
+            //Se limpia la pantalla antes de mostar los datos
             Console.Clear();
-
+            //Area de impresion de los datos
             Console.WriteLine("<<<<<<<<<<<<<*INFORMACION PERSONAL*>>>>>>>>>>>>>");
             Console.Write("\n    Nombre(s): Miguel Angel");
             Console.Write("\n    Apellido(s): Garcia Desena");
@@ -26,7 +29,7 @@ namespace Ejercicio1
             Console.Write("\n    Direcion: C/ Manolo Ortiz, #03, Los Block, Villa Central");
             Console.Write("\n    Lugar de trabajo: UCATEBA");
             Console.Write("\n    Sueldo: N/A");
-
+            //Pausa durante la ejecucion del programa
             Console.ReadKey();
         }
     }
---
+++ EjerciciosNivelacion/Ejercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Programa que suma dos numeros
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asignacion del color de las letras y el fundo de la consola
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            //Se limpia la pantalla antes de mostrar la informacion en la misma
+            Console.Clear();
+            //Area de declaracion de variables
+            double v1, v2, r;
+
+            Console.WriteLine("-------------<Suma de dos nuemros>-------------");//Titulo del programa
+            Console.Write("\nIngrese el primer valor: ");//Mensaje para el usuario
+            v1 = Convert.ToDouble(Console.ReadLine());//Captuda de datos por teclado y conversion por clase Convert
+            Console.Write("Ingrese el segundo valor: ");//Mensaje para el usuario
+            v2 = double.Parse(Console.ReadLine());//Captura de datos por teclado y conversion por Parseo
+
+            r = v1 + v2;//Se suman los datos
+
+            Console.Write("\nEl resultado es: {0}", r);//Se muestra el resultado formateado a un decimal
+
+            Console.ReadKey();//Pausa durante la ejecucion del programa
+        }
+    }
+}
