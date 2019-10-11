245cd82c4bf9e5b4e3205c72937095cde79ef640
MikeGarcia03 <miguelangelgarciadesena@gmail.com>
2019-10-10 23:22:45 -0400

    Creacion de los proyectos Ejercicio2_5 y Ejercicio2_6, este ultimo sin terminar.

---
+++ EjerciciosNivelacion/Ejercicio2_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Programa que convierte de centimenos a purgada
+namespace Ejercicio2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables a utilizar en el programa
+            double r, cm, PUL=2.54;
+
+            Console.WriteLine(" Conversor de centimentros a pulgadas");//Titulo descriptivo del programa
+            try //Manejo de excepciones en caso de errores
+            {
+                Console.Write("\nIngrese los centimetros: ");//Mensaje para el usuario
+                cm = double.Parse(Console.ReadLine());//Captura de datos por teclado convertidos por parseo
+
+                r = cm / PUL;//Conversion de centimetros a pulgadas
+
+                Console.Write("\n{0:N2} cm. es igual a {1:N2} pulg.", cm, r);//Impresion del resultado
+            }
+            catch (Exception error) //Captura de la excepcion en caso de error
+            {
+                Console.WriteLine(error.Message);//Impresion del mensaje de error
+            }
+
+            Console.ReadKey();//Pausa durate la ejecucion del programa
+        }
+    }
+}
---
+++ EjerciciosNivelacion/Ejercicio2_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Programa que calcula la cuota de un prestamo simple
+namespace Ejercicio2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaracion de variables a utilizar en el programa
+            int tiempo;
+            double capital, interes, prestamo, cuota;
+
+            Console.WriteLine(" Calculadora de prestamos simple");//Titulo descriptivo del programa
+            try
+            {
+                Console.Write("\nIngrese la cantidad del prestamo: ");//Mensaje para el usuario
+                capital = double.Parse(Console.ReadLine());//Captura de datos por teclado convertidos por parseo
+                Console.Write("Diga el tiempo del prestamo(mese): ");
+                tiempo = int.Parse(Console.ReadLine());
+                Console.Write("Especifique la tasa de interes(porcentual): ");
+                interes = double.Parse(Console.ReadLine());
+
+                prestamo = capital + ((capital * interes)/100);
+            }
+            catch
+            {
+            }            
+        }
+    }
+}
